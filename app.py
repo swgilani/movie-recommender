d3e3bd422802b81d54d07bbcb36c8e1b119d5fe0
Wasiq <iwasiqiqbal@gmail.com>
2020-12-27 22:26:37 -0500

    first

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+import json
+import requests
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:42024@localhost/postgres'
+app.config['SECRET_KEY'] = 'super-secret'
+app.config['SECURITY_REGISTERABLE']=True
+app.config['SECURITY_PASSWORD_SALT'] = 'secret-string'
+app.debug = True
+db = SQLAlchemy(app)
+
+
+
+
+
+@app.route('/')
+def index():
+    response = requests.get("https://api.themoviedb.org/3/movie/157336/recommendations?api_key=586f9b611ec26170fbc7b228645fa5ca")
+    json_data = json.loads(response.text)
+    return json_data
+
+if __name__ == "__main__":
+    app.run()
